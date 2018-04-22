ae1268c827be08fc0dd64d4ade63b249a4102bee
rohandhi <38591385+rohandhi@users.noreply.github.com>
2018-04-23 02:09:30 +0530

    Update opencv_Object tracking.py

--- opencv_Object tracking.py
+++ opencv_Object tracking.py
@@ -1,21 +1,18 @@
 import cv2
 import numpy as np
-import imutils
 
 cam = cv2.VideoCapture(0)
 
 while True:
-    _,frame=cam.read()
-    frame=imutils.resize(frame,width=500)
+    _,img=cam.read()
+    img=cv2.flip(img,1)
 
-    hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
-    lower_blue= np.array([100,100,100])
-    upper_blue= np.array([120,255,255])
+    lower= np.array([0,0,0])
+    upper= np.array([0,0,255])
 
-    mask= cv2.inRange(hsv, lower_blue, upper_blue)
-    mask= cv2.erode(mask,None,iterations=1)
-    mask= cv2.dilate(mask,None,iterations=1)
+    mask= cv2.inRange(hsv, lower, upper)
 
     cont=cv2.findContours(mask.copy(),cv2.RETR_EXTERNAL,
                           cv2.CHAIN_APPROX_SIMPLE)[-2]
@@ -24,16 +21,17 @@ while True:
         
         c = max(cont, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
         if radius > 10:
-            cv2.circle(frame, (int(x), int(y)), int(radius),(0,0,255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+            cv2.circle(img, (int(x), int(y)), int(radius),(0,0,255),2)
+            cv2.circle(img, center, 5, (0, 0, 255), -1)
+##            print len(center)
 
+##    print len(cont)
     
-    res=cv2.bitwise_and(frame,frame,mask=mask)
-    cv2.imshow('LIVE',frame)
+    res=cv2.bitwise_and(img,img,mask=mask)
+    
+    cv2.imshow('LIVE',img)
 ##    cv2.imshow('BLUE',res)
 
     
