63c279b5333500ecb8d9b6aa09e052434e77cdb6
rohandhi <38591385+rohandhi@users.noreply.github.com>
2018-04-23 13:32:21 +0530

    Update opencv_Object tracking.py

--- opencv_Object tracking.py
+++ opencv_Object tracking.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 cam = cv2.VideoCapture(0)
-
+point=[]
 while True:
     _,img=cam.read()
     img=cv2.flip(img,1)
@@ -21,14 +21,27 @@ while True:
         
         c = max(cont, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
-
+        M = cv2.moments(c)
+##        print M
+        cx= int(M["m10"] / M["m00"])
+        cy= int(M["m01"] / M["m00"])
+        center = (cx,cy)
+        
+        if center==None:
+            continue
+        
         if radius > 10:
-            cv2.circle(img, (int(x), int(y)), int(radius),(0,0,255),2)
+            cv2.circle(img, (int(x), int(y)), int(radius),(0,255,255),2)
             cv2.circle(img, center, 5, (0, 0, 255), -1)
+##            print radius
+##            print center
 ##            print len(center)
-
+    point.append(center)
+##    print point
 ##    print len(cont)
-    
+    if point is not None:
+        for i in range(1,len(point)):
+            cv2.line(img,point[i-1],point[i],(255,0,0),5)
     res=cv2.bitwise_and(img,img,mask=mask)
     
     cv2.imshow('LIVE',img)
